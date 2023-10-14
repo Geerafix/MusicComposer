d5790c1c1b5ff465437ae95278fc796f13afd5c7
Geerafix <geerafix@gmail.com>
2023-10-14 13:51:58 +0200

    optimize code

--- src/MusicComposer.java
+++ src/MusicComposer.java
@@ -26,19 +26,15 @@ public class MusicComposer {
     }
 
     public static void title() throws MidiUnavailableException, InterruptedException, IOException {
-        int y = 0;
         boolean run = true;
 
         screen.clear();
-
-        reader(new Scanner(new File("scenes/title.txt")), 10, y);
+        reader(new Scanner(new File("scenes/title.txt")), 10, 0);
         put(80, 28, "by Adam Grzeszczuk");
-
         screen.refresh();
 
         while (run) {
             Key key = screen.readInput();
-
             while (key == null) {
                 key = screen.readInput();
             }
@@ -64,18 +60,12 @@ public class MusicComposer {
         StringBuilder filename = new StringBuilder("");
         Synthesizer synth = MidiSystem.getSynthesizer();
         MidiChannel[] channels = synth.getChannels();
-        int channel = 0, y = 0, position = 0, currentNote = 1, currentDuration = 200, lastChar = 11;
-        int trackY = 4;
-        int trackX = 75;
+        int channel = 0, y = 0, trackX = 75, trackY = 4, position = 0, currentNote = 1, currentDuration = 200, lastChar = 11;
         boolean run = true;
 
         synth.open();
         screen.clear();
-
-        reader(new Scanner(new File("scenes/compose.txt")), 2, y+ 3);
-
-        screen.refresh();
-
+        reader(new Scanner(new File("scenes/compose.txt")), 2, y + 3);
         noteToASCIIArt(noteConv(notes[currentNote]));
         durationToASCIIArt(currentDuration);
         put(2, 1, "Filename: ");
@@ -85,7 +75,6 @@ public class MusicComposer {
 
         while (run) {
             Key key = screen.readInput();
-
             while (key == null) {
                 key = screen.readInput();
             }
@@ -97,7 +86,6 @@ public class MusicComposer {
                     } else {
                         --currentNote;
                     }
-
                     clearNote();
                     noteToASCIIArt(noteConv(notes[currentNote]));
                     screen.refresh();
@@ -116,7 +104,6 @@ public class MusicComposer {
                     if (currentDuration != 5000) {
                         currentDuration += 50;
                     }
-
                     clearDuration();
                     durationToASCIIArt(currentDuration);
                     screen.refresh();
@@ -139,44 +126,26 @@ public class MusicComposer {
                         clearDuration();
                         noteToASCIIArt(noteConv(notes[currentNote]));
                         durationToASCIIArt(currentDuration);
-
                         put(80, 1, Integer.toString(position + 1));
                         put(97, 1, Integer.toString(track.size()));
-
                     } else if (position < track.size()) {
                         track.set(position, new Note(notes[currentNote], currentDuration));
                     }
-
-                    clearTrack();
-
-                    for (int i = 0; i < track.size(); i++) {
-                        if (trackY == 29) {
-                            trackX += 13;
-                            trackY = 4;
-                        }
-                        put(trackX, trackY, (i + 1) + ". ");
-                        put((trackX + 3), trackY, noteConv(track.get(i).getNumber()));
-                        put((trackX + 7), trackY, Integer.toString(track.get(i).getDuration()));
-                        trackY += 1;
-                    }
-                    trackY = 4;
-                    trackX = 75;
+                    refreshTrack(trackX, trackY, track);
                     screen.refresh();
                     break;
                 case Escape:
-                    for (Note note : track) {
-                        channels[channel].noteOn(note.getNumber(), 100);
-                        Thread.sleep(note.getDuration());
-                        channels[channel].noteOff(note.getNumber());
-                    }
-
+                    // for (Note note : track) {
+                    //     channels[channel].noteOn(note.getNumber(), 100);
+                    //     Thread.sleep(note.getDuration());
+                    //     channels[channel].noteOff(note.getNumber());
+                    // }
                     title();
                     break;
                 case Insert:
                     channels[channel].noteOn(notes[currentNote], 100);
                     Thread.sleep(currentDuration);
                     channels[channel].noteOff(notes[currentNote]);
-
                     break;
                 case Home:
                     if (position > 0) {
@@ -188,17 +157,14 @@ public class MusicComposer {
                         noteToASCIIArt(noteConv(track.get(position).getNumber()));
                         durationToASCIIArt(track.get(position).getDuration());
                     }
-
                     put(80, 1, "   ");
                     put(80, 1, Integer.toString(position + 1));
-
                     screen.refresh();
                     break;
                 case End:
                     if (position < track.size()) {
                         position += 1;
                     }
-
                     if (position < track.size()) {
                         currentNote = track.get(position).getNumber() - 23;
                         currentDuration = track.get(position).getDuration();
@@ -207,7 +173,6 @@ public class MusicComposer {
                         noteToASCIIArt(noteConv(track.get(position).getNumber()));
                         durationToASCIIArt(track.get(position).getDuration());
                     }
-
                     if (position == track.size()) {
                         currentNote = 1;
                         currentDuration = 200;
@@ -216,48 +181,30 @@ public class MusicComposer {
                         noteToASCIIArt(noteConv(notes[currentNote]));
                         durationToASCIIArt(currentDuration);
                     }
-
                     put(80, 1, "   ");
                     put(80, 1, Integer.toString(position + 1));
-
                     screen.refresh();
                     break;
                 case Delete:
                     if (position < track.size()) {
                         track.remove(position);
-                        clearNote();
-                        clearDuration();
                         currentNote = 1;
                         currentDuration = 200;
+                        position = track.size();
+                        clearNote();
+                        clearDuration();
                         noteToASCIIArt(noteConv(notes[currentNote]));
                         durationToASCIIArt(currentDuration);
-                        position = track.size();
                         put(97, 1, "   ");
                         put(97, 1, Integer.toString(track.size()));
                         put(80, 1, Integer.toString(position + 1));
+                        refreshTrack(trackX, trackY, track);
                         screen.refresh();
-
-                        clearTrack();
-
-                        for (int i = 0; i < track.size(); i++) {
-                            if (trackY == 29) {
-                                trackX += 13;
-                                trackY = 4;
-                            }
-                            put(trackX, trackY, (i + 1) + ". ");
-                            put((trackX + 3), trackY, noteConv(track.get(i).getNumber()));
-                            put((trackX + 7), trackY, Integer.toString(track.get(i).getDuration()));
-                            trackY += 1;
-                        }
-                        trackY = 4;
-                        trackX = 75;
                     }
-
                     break;
                 case NormalKey:
                     if (lastChar < 27) {
                         filename.append(key.getCharacter());
-                        ;
                         put(12, 1, filename.toString());
                         lastChar++;
                     }
@@ -283,7 +230,6 @@ public class MusicComposer {
                     break;
                 default:
                     break;
-
             }
         }
     }
@@ -297,7 +243,6 @@ public class MusicComposer {
 
         screen.clear();
         synth.open();
-
         reader(new Scanner(new File("scenes/tracks.txt")), 11, y);
 
         for (int trackNr = 0; trackNr < tracks.length; trackNr++) {
@@ -307,15 +252,12 @@ public class MusicComposer {
                     Terminal.Color.BLACK,
                     ScreenCharacterStyle.Bold);
         }
-
         screen.putString(12, select + 10, ">",
                 Terminal.Color.GREEN, Terminal.Color.BLACK, ScreenCharacterStyle.Bold);
-
         screen.refresh();
 
         while (run) {
             Key key = screen.readInput();
-
             while (key == null) {
                 key = screen.readInput();
             }
@@ -325,8 +267,7 @@ public class MusicComposer {
                     title();
                     break;
                 case ArrowUp:
-                    screen.putString(12, select + 10, " ",
-                            Terminal.Color.WHITE, Terminal.Color.BLACK, ScreenCharacterStyle.Bold);
+                    put(12, select + 10, " ");
                     if (select == 0) {
                         select = tracks.length - 1;
                     } else {
@@ -337,8 +278,7 @@ public class MusicComposer {
                     screen.refresh();
                     break;
                 case ArrowDown:
-                    screen.putString(12, select + 10, " ",
-                            Terminal.Color.WHITE, Terminal.Color.BLACK, ScreenCharacterStyle.Bold);
+                    put(12, select + 10, " ");
                     if (select == tracks.length - 1) {
                         select = 0;
                     } else {
@@ -367,7 +307,7 @@ public class MusicComposer {
                     }
                     break;
                 case Insert:
-                    edit(toArray(tracks[select].getName()), "tracks/" + tracks[select].getName());
+                    edit(toList(tracks[select].getName()), "tracks/" + tracks[select].getName());
                     break;
                 default:
                     break;
@@ -378,13 +318,9 @@ public class MusicComposer {
     public static void edit(ArrayList<Note> track, String fname)
             throws FileNotFoundException, IOException, MidiUnavailableException, InterruptedException {
         boolean run = true;
-        int currentNote = track.get(0).getNumber() - 23,
-                currentDuration = track.get(0).getDuration(),
-                y = 0, trackY = 4, trackX = 75,
-                position = 0;
+        int currentNote = track.get(0).getNumber() - 23, currentDuration = track.get(0).getDuration(), position = 0, y = 0,  trackX = 75, trackY = 4;
 
-        screen.clear();
-            
+        screen.clear();         
         reader(new Scanner(new File("scenes/edit.txt")), 2, y + 3);
 
         for (int i = 0; i < track.size(); i++) {
@@ -402,7 +338,6 @@ public class MusicComposer {
 
         noteToASCIIArt(noteConv(track.get(0).getNumber()));
         durationToASCIIArt(track.get(0).getDuration());
-
         put(2, 1, "Filename: " + fname.substring(7, fname.length() - 4));
         put(70, 1, "Position: " + (position + 1));
         put(85, 1, "Note count: " + (track.size()));
@@ -410,7 +345,6 @@ public class MusicComposer {
 
         while (run) {
             Key key = screen.readInput();
-
             while (key == null) {
                 key = screen.readInput();
             }
@@ -422,7 +356,6 @@ public class MusicComposer {
                     } else {
                         --currentNote;
                     }
-
                     clearNote();
                     noteToASCIIArt(noteConv(notes[currentNote]));
                     screen.refresh();
@@ -441,7 +374,6 @@ public class MusicComposer {
                     if (currentDuration != 5000) {
                         currentDuration += 50;
                     }
-
                     clearDuration();
                     durationToASCIIArt(currentDuration);
                     screen.refresh();
@@ -458,21 +390,7 @@ public class MusicComposer {
                     if (position < track.size()) {
                         track.set(position, new Note(notes[currentNote], currentDuration));
                     }
-
-                    clearTrack();
-
-                    for (int i = 0; i < track.size(); i++) {
-                        if (trackY == 29) {
-                            trackX += 13;
-                            trackY = 4;
-                        }
-                        put(trackX, trackY, (i + 1) + ". ");
-                        put((trackX + 3), trackY, noteConv(track.get(i).getNumber()));
-                        put((trackX + 7), trackY, Integer.toString(track.get(i).getDuration()));
-                        trackY += 1;
-                    }
-                    trackY = 4;
-                    trackX = 75;
+                    refreshTrack(trackX, trackY, track);
                     screen.refresh();
                     break;
                 case Home:
@@ -484,18 +402,15 @@ public class MusicComposer {
                         clearDuration();
                         noteToASCIIArt(noteConv(track.get(position).getNumber()));
                         durationToASCIIArt(track.get(position).getDuration());
+                        put(80, 1, "   ");
+                        put(80, 1, Integer.toString(position + 1));
                     }
-
-                    put(80, 1, "   ");
-                    put(80, 1, Integer.toString(position + 1));
-                    
                     screen.refresh();
                     break;
                 case End:
                     if (position < track.size() - 1) {
                         position += 1;
                     }
-
                     if (position < track.size()) {
                         currentNote = track.get(position).getNumber() - 23;
                         currentDuration = track.get(position).getDuration();
@@ -504,19 +419,17 @@ public class MusicComposer {
                         noteToASCIIArt(noteConv(track.get(position).getNumber()));
                         durationToASCIIArt(track.get(position).getDuration());
                     }
-
                     put(80, 1, "   ");
                     put(80, 1, Integer.toString(position + 1));
-
                     screen.refresh();
                     break;
                 case Enter:
-                    PrintWriter pw = new PrintWriter(fname);
+                    PrintWriter toFile = new PrintWriter(fname);
                     for (Note note : track) {
-                        pw.println(Integer.toString(note.getNumber()));
-                        pw.println(Integer.toString(note.getDuration()));
+                        toFile.println(Integer.toString(note.getNumber()));
+                        toFile.println(Integer.toString(note.getDuration()));
                     }
-                    pw.close();
+                    toFile.close();
                     break;
                 case Escape:
                     tracks();
@@ -529,27 +442,22 @@ public class MusicComposer {
 
     public static String noteConv(int noteNumber) {
         String[] noteNames = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-        int octave = noteNumber / 12;
-        int noteInOctave = noteNumber % 12;
-
+        int octave = noteNumber / 12, noteInOctave = noteNumber % 12;
         if (octave - 1 < 0) {
             return "-";
         }
-
         return noteNames[noteInOctave] + (octave - 1);
     }
 
     private static void noteToASCIIArt(String stream) throws FileNotFoundException {
         String note = String.valueOf(stream);
         Scanner scan;
-        int x = 3, y;
+        int x = 3, y = 12;
         for (char digit : note.toCharArray()) {
             scan = new Scanner(new File("characters/" + digit + ".txt"));
-            y = 12;
             reader(scan, x, y);
-            scan.close();
-            screen.refresh();
             x = x + 9;
+            screen.refresh();
         }
     }
 
@@ -559,9 +467,7 @@ public class MusicComposer {
         int x = 34, y = 12;
         for (char digit : note.toCharArray()) {
             scan = new Scanner(new File("characters/" + digit + ".txt"));
-            y = 12;
             reader(scan, x, y);
-            scan.close();
             screen.refresh();
             x = x + 9;
         }
@@ -570,7 +476,7 @@ public class MusicComposer {
     private static void clearNote() {
         int x = 3, y = 12;
         while (y < 17) {
-            put(x, y, "                          ");
+            put(x, y, "                           ");
             y++;
         }
     }
@@ -583,19 +489,28 @@ public class MusicComposer {
         }
     }
 
-    private static void clearTrack() {
+    public static void refreshTrack(int trackX, int  trackY, ArrayList<Note> track) {
         for (int y = 0; y < 25; y++) {
             put(75, y + 4, "                               ");
         }
+        for (int i = 0; i < track.size(); i++) {
+            if (trackY == 29) {
+                trackX += 13;
+                trackY = 4;
+            }
+            put(trackX, trackY, (i + 1) + ". ");
+            put((trackX + 3), trackY, noteConv(track.get(i).getNumber()));
+            put((trackX + 7), trackY, Integer.toString(track.get(i).getDuration()));
+            trackY += 1;
+        }
     }
 
-    public static ArrayList<Note> toArray(String filename) throws FileNotFoundException {
+    public static ArrayList<Note> toList(String filename) throws FileNotFoundException {
         ArrayList<Note> track = new ArrayList<>();
         Scanner scan = new Scanner(new File("tracks/" + filename));
         while (scan.hasNext()) {
             track.add(new Note(Integer.parseInt(scan.nextLine()), Integer.parseInt(scan.nextLine())));
         }
-        scan.close();
         return track;
     }
 
